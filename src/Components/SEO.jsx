3082b832715c9c2988afb40bb9c97f99d95c5956
Kiril Gaydarov <kikchan@users.noreply.github.com>
2025-03-11 16:31:54 +0100

    fix: Replaced react-helmet with react-helmet-async

--- src/Components/SEO.jsx
+++ src/Components/SEO.jsx
@@ -1,10 +1,12 @@
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export const SEO = ({ name, ocupation, description }) => {
   return (
-    <Helmet>
-      <title>{name} - {ocupation}</title>
-      <meta name="description" content={description} />
-    </Helmet>
+    <HelmetProvider>
+      <Helmet>
+        <title>{name} - {ocupation}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+    </HelmetProvider>
   );
 };
